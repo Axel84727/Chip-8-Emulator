3736e5b0da4fd963c3a07c92bc0e1861705d9761
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-12-06 19:35:59 -0300

    test: added a simple test for cpu in main.cpp
    -refactor cpu.cpp with modern initializers

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -1,19 +1,15 @@
-#include "cpu.hpp"
+#include "cpu/cpu.hpp"
 
 cpu::cpu()
+    : opcode(0),
+      I(0),
+      delay(0),
+      pc(0x200),
+      sound_timers(0),
+      stack_pointer(0),
+      V{0},
+      stack{0}
 {
-    opcode = 0;
-    I = 0;
-    delay = 0;
-    pc = 0x200;
-    sound_timers = 0;
-    stack_pointer = 0;
-
-    for (int i = 0; i < 16; ++i)
-    {
-        V[i] = 0;
-        stack[i] = 0;
-    }
 }
 
 cpu::~cpu()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "cpu/cpu.hpp"
+int main()
+{
+    cpu chip8;
+
+    std::cout << "CPU created successfully\n";
+}
