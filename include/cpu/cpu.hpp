a05ecd4d05ce2caeb5b0f990049cb1babe3aec9f
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-12-06 19:20:08 -0300

    refactor(cpu): transformed cpu from a struct to a class
    - this is due to advantage of encapsulation

--- include/cpu/cpu.hpp
+++ include/cpu/cpu.hpp
@@ -1,16 +1,19 @@
 #pragma once
 #include <cstdint>
 
-struct cpu
+class cpu
 {
+private:
     std::uint16_t opcode;
-    std::uint16_t I; // This register is generally used to store memory addresses
+    std::uint16_t I;
     std::uint8_t delay;
-    std::uint16_t pc; // is used to store the currently executing address.
+    std::uint16_t pc;
     std::uint8_t sound_timers;
-    std::uint8_t stack_pointer; // it is used to point to the topmost level of the stack.
-    std::uint8_t V[16];         // Chip-8 has 16 general purpose 8-bit registers, usually referred to as Vx
+    std::uint8_t stack_pointer;
+    std::uint8_t V[16];
     std::uint16_t stack[16];
 
-    cpu::cpu();
+public:
+    cpu();
+    ~cpu();
 };
--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -8,9 +8,14 @@ cpu::cpu()
     pc = 0x200;
     sound_timers = 0;
     stack_pointer = 0;
+
     for (int i = 0; i < 16; ++i)
     {
         V[i] = 0;
         stack[i] = 0;
     }
+}
+
+cpu::~cpu()
+{
 }
