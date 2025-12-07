6f9ea8dfd14805186d9c04ed15955dffeb6684d0
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-12-06 21:05:20 -0300

    refactor: Decouple CPU and Memory ownership
    - Removes redundant total_memory array from cpu.hpp.
    - Updates cpu.hpp to hold a pointer to the memory object (*memory_ptr*).
    - Updates src/cpu/cpu.cpp to initialize memory_ptr via constructor.

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -1,17 +1,30 @@
-#include "cpu/cpu.hpp"
+#include "cpu.hpp"
+#include <cstring> // For std::memset or std::fill
 
-cpu::cpu()
+cpu::cpu(memory *mem_ptr)
     : opcode(0),
       I(0),
-      delay(0),
-      pc(0x200),
-      sound_timers(0),
+      pc(0x200), // Execution starts at 0x200
+      delay_timer(0),
+      sound_timer(0),
       stack_pointer(0),
-      V{0},
-      stack{0}
+      memory_ptr(mem_ptr) // Assign memory pointer
 {
+    // Initialize V registers and stack to zero
+    std::memset(V, 0, sizeof(V));
+    std::memset(stack, 0, sizeof(stack));
 }
 
-cpu::~cpu()
+void cpu::emulate_cycle()
 {
+    // 1. FETCH (Get Opcode)
+    std::uint16_t byte1 = memory_ptr->read_byte(pc);
+    std::uint16_t byte2 = memory_ptr->read_byte(pc + 1);
+
+    opcode = (byte1 << 8) | byte2; // Big-Endian: MSB + LSB
+
+    switch (opcode & 0xF000)
+    {
+    case 0x1000:
+        }
 }
